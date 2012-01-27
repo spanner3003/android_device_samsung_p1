73fe584634831bb0cf8e4c1bec56bc75adf3e542
Chirayu Desai <chirayudesai1@gmail.com>
2012-01-27 18:55:27 +0530

    VENDOR_SAMSUNG_DIR moved
    Change-Id: Ic35cd052595f5bb4a0dbd8d2c99328e1e0ad531d
    
    Conflicts:
    
    	releasetools/galaxytab_edify_generator.py

--- releasetools/galaxytab_edify_generator.py
+++ releasetools/galaxytab_edify_generator.py
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,7 +17,6 @@ import os, sys
 
 LOCAL_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 RELEASETOOLS_DIR = os.path.abspath(os.path.join(LOCAL_DIR, '../../../build/tools/releasetools'))
-VENDOR_SAMSUNG_DIR = os.path.abspath(os.path.join(LOCAL_DIR, '../../../vendor/samsung'))
 
 import edify_generator
 
@@ -34,10 +33,11 @@ class EdifyGenerator(edify_generator.EdifyGenerator):
       self.script.append(
             ('package_extract_file("busybox", "/tmp/busybox");\n'
              'set_perm(0, 0, 0777, "/tmp/busybox");'))
+             
       self.script.append(
             ('package_extract_file("redbend_ua", "/tmp/redbend_ua");\n'
              'set_perm(0, 0, 0777, "/tmp/redbend_ua");'))
- 
+
       self.script.append('package_extract_file("zImage", "/tmp/zImage");')
 
       self.script.append('package_extract_file("updater.sh", "/tmp/updater.sh");')
