f3864709cb6d49beb753eebb38a9b13b540d0fb3
spanner3003 <spanner3003@gmail.com>
2012-02-18 23:27:49 +0000

    add logging to see what i can see
    Change-Id: Ie7e059154df5ba1610eabbb4bf8e0ca72a01f689

--- P1Parts/src/com/cyanogenmod/P1Parts/Startup.java
+++ P1Parts/src/com/cyanogenmod/P1Parts/Startup.java
@@ -3,6 +3,7 @@ package com.cyanogenmod.P1Parts;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class Startup extends BroadcastReceiver {
 
@@ -11,6 +12,7 @@ public class Startup extends BroadcastReceiver {
         ColorTuningPreference.restore(context);
         TouchKeyBacklightTimeout.restore(context);
         Hspa.restore(context);
+        Log.d("P1Parts",context.toString() );
     }
 
 }
