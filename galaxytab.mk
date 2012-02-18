81b21408363ccbc7ed4b2e7a9f427e4ea629fcf3
Cameron Ross <spanner3003@gmail.com>
2012-02-18 12:18:09 +0000

    added faclock here now.

--- galaxytab.mk
+++ galaxytab.mk
@@ -131,6 +131,27 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	device/samsung/galaxytab/Credits-CM.html:system/etc/Credits-CM.html
 
+#FACELOCK
+PRODUCT_COPY_FILES +=  \
+    device/samsung/galaxytab/prebuilt/facelock/app/FaceLock.apk:system/app/FaceLock.apk \
+    device/samsung/galaxytab/prebuilt/facelock/lib/libfacelock_jni.so:system/lib/libfacelock_jni.so \
+    device/samsung/galaxytab/prebuilt/facelock/lib/libfilterpack_facedetect.so:system/lib/libfilterpack_facedetect.so \
+    device/samsung/galaxytab/prebuilt/facelock/lib/libflint_engine_jni_api.so:system/lib/libflint_engine_jni_api.so \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-r0-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-r0-ri20.2d_n2/full_model.bin \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-rn7-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-rn7-ri20.2d_n2/full_model.bin \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-rp7-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/left_eye-y0-yi45-p0-pi45-rp7-ri20.2d_n2/full_model.bin \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/nose_base-y0-yi45-p0-pi45-r0-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/nose_base-y0-yi45-p0-pi45-r0-ri20.2d_n2/full_model.bin \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/nose_base-y0-yi45-p0-pi45-rn7-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/nose_base-y0-yi45-p0-pi45-rn7-ri20.2d_n2/full_model.bin \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/nose_base-y0-yi45-p0-pi45-rp7-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/nose_base-y0-yi45-p0-pi45-rp7-ri20.2d_n2/full_model.bin \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/right_eye-y0-yi45-p0-pi45-r0-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/right_eye-y0-yi45-p0-pi45-r0-ri20.2d_n2/full_model.bin \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/right_eye-y0-yi45-p0-pi45-rn7-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/right_eye-y0-yi45-p0-pi45-rn7-ri20.2d_n2/full_model.bin \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/right_eye-y0-yi45-p0-pi45-rp7-ri20.2d_n2/full_model.bin:system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.3/right_eye-y0-yi45-p0-pi45-rp7-ri20.2d_n2/full_model.bin \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/detection/yaw_roll_face_detectors.3/head-y0-yi45-p0-pi45-r0-ri30.4a/full_model.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.3/head-y0-yi45-p0-pi45-r0-ri30.4a/full_model.bin \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/detection/yaw_roll_face_detectors.3/head-y0-yi45-p0-pi45-rn30-ri30.5/full_model.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.3/head-y0-yi45-p0-pi45-rn30-ri30.5/full_model.bin \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/detection/yaw_roll_face_detectors.3/head-y0-yi45-p0-pi45-rp30-ri30.5/full_model.bin:system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.3/head-y0-yi45-p0-pi45-rp30-ri30.5/full_model.bin \
+    device/samsung/galaxytab/prebuilt/facelock/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N/full_model.bin:system/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N/full_model.bin
+
+
 # Tablet!
 PRODUCT_CHARACTERISTICS := tablet
 
@@ -207,30 +228,11 @@ endif
 PRODUCT_COPY_FILES += \
 	$(LOCAL_KERNEL):kernel
 
-# It's a Tablet!
-$(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
-
-$(call inherit-product, vendor/aokp/products/common.mk)
-
-$(call inherit-product-if-exists, vendor/samsung/galaxytab/galaxytab-vendor.mk)
-
 # copy the filesystem converter
 PRODUCT_COPY_FILES += \
 	device/samsung/galaxytab/updater.sh:updater.sh
 
-# Setup device specific product configuration.
-#PRODUCT_NAME := aokp_galaxytab
-#PRODUCT_BRAND := google
-#PRODUCT_DEVICE := galaxytab
-#PRODUCT_MODEL := GT-P1000
-#PRODUCT_MANUFACTURER := samsung
-#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-P1000 TARGET_DEVICE=GT-P1000 BUILD_ID=GINGERBREAD BUILD_FINGERPRINT=samsung/GT-P1000/GT-P1000:2.3.5/GINGERBREAD/XWJQ3:user/release-keys PRIVATE_BUILD_DESC="GT-P1000-user 2.3.5 GINGERBREAD XWJQ3 release-keys"
+# It's a Tablet!
+$(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
 
-#TARGET_OTA_ASSERT_SKIP := true
-
-# Inherit common build.prop overrides
--include vendor/aokp/products/common_versions.mk
-
-# Copy maguro specific prebuilt files
-PRODUCT_COPY_FILES += \
-	vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip \
+$(call inherit-product-if-exists, vendor/samsung/galaxytab/galaxytab-vendor.mk)
